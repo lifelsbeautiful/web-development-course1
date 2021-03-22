00ba1d26d8fa17043a772737821cf52e22fc6da7
Vanya Mihaylov <ivan_mikhaylov_2002@mail.ru>
2021-03-22 19:44:25 +0300

    Adding code in js Mihaylov Ivan

--- lw3/task3/main.js
+++ lw3/task3/main.js
@@ -11,30 +11,28 @@ var MusicPlayer = {
   },
   play: function () {
     this.status = 'play';
+    console.log('Play');
   },
   pause: function () {
     this.status = 'pause';
+    console.log('Pause');
   },
   next: function () {
-    // TODO: проиграть следующую композицию
+    this.currentTrack 1 >= this.trackList.length + 1;
   },
   prev: function () {
-    // TODO: проиграть предыдущую композицию
+    this.currentTrack <= 0 this.trackList.length - 1;
   },
   showTracks: function () {
-    // TODO: вывести в console.log список треков
-    // в формате:
-    // 1. song1.mp3
-    // 2. song2.mp3 - играет сейчас
-    // 3. song3.mp3
-  }
-};
+    console.log(this.trackList);
+    console.log(this.currentTrack + ' - Сейчас звучит');
+  };
 
-Player.display(); // "Track: song1.mp3, Status: pause"
-Player.play();
-Player.display(); // "Track: song1.mp3, Status: play"
+  Player.display(); // "Track: song1.mp3, Status: pause"
+  Player.play();
+  Player.display(); // "Track: song1.mp3, Status: play"
 
-Player.next(); // переключает другой трек
-Player.display(); // "Track: song2.mp3, Status: play"
+  Player.next(); // переключает другой трек
+  Player.display(); // "Track: song2.mp3, Status: play"
 
-Player.showTracks(); // вывести в консоль список доступных треков
+  Player.showTracks(); // вывести в консоль список доступных треков
