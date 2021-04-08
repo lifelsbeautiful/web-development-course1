a09922f45fd3ca56168a7081679a1fc03c023831
Vanya Mihaylov <ivan_mikhaylov_2002@mail.ru>
2021-04-08 09:20:35 +0300

    add html and css by Mihaylov Ivan

--- lw4/task3/script.js
+++ lw4/task3/script.js
@@ -1,30 +1,6 @@
-let array1 = [1, [2, 3, 4], 5];
+var arr = ["Яблоко", "Апельсин", "Груша"];
 
-function flatArray(arrayParam1) {
-  if (typeof arrayParam1 === 'object') {
-    let arrayAnswer = [];
-    function isNumber(num) {
-      if (typeof num == 'number') {
-        arrayAnswer.push(num);
-      }
-    }
-    if (typeof arrayParam1 == 'object') {
-      for (let i = 0; i < arrayParam1.length; i++) {
-        if (isNaN(arrayParam1[i]) === false) {
-          isNumber(arrayParam1[i]);
-        }
-
-        if (typeof arrayParam1[i] == 'object' && arrayParam1[i] !== null) {
-          let numArray = i;
-          for (let i = 0; i < arrayParam1[numArray].length; i++) {
-            isNumber(arrayParam1[numArray][i]);
-          }
-        }
-      }
-    }
-    return arrayAnswer;
-  }
-  else {
-    return "Вы передаете не массив";
-  }
-}
+arr.forEachRight(function (arr) {
+  arr.reverse();
+  alert(arr);
+});
